ae4958a354c9106082222a3dd05fb488c8ecb333
maousama1432 <131804292+maousama1432@users.noreply.github.com>
2024-03-24 19:51:29 -0400

    atualização

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,18 @@
 export default async function Home() {
   const url = "http://localhost:3000/api";
 
-  const resposta = await fetch(url, {
-    cache: "no-cache"
+  const resposta = await fetch("https://back-end-ifms.vercel.app/campi", {
+    next: {
+      revalidate: 1
+    }
   });
+  const campus = await resposta.json();
 
-  // Verifica se a resposta foi bem-sucedida (status 200)
-  if (!resposta.ok) {
-    throw new Error(`Erro ao carregar dados: ${resposta.status} - ${resposta.statusText}`);
-  }
-
-  // Verifica se a resposta não está vazia
-  const json = await resposta.json();
-  if (!json) {
-    throw new Error("Resposta vazia ou inválida");
-  }
-
-  // Continua o processamento normal
   return (
     <main>
       <h1>Home</h1>
-      {json.map((campi) =>
-        <div key={campi.id}>
+      {campus.map((campi) =>
+        <div>
           <p>{campi.nome_campi}</p>
         </div>
       )}
